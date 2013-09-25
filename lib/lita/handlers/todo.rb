91b8d2da26f0ffdb0be104949adf035f93579bbf
glebtv <glebtv@gmail.com>
2013-09-26 00:03:05 +0400

    works

--- lib/lita/handlers/todo.rb
+++ lib/lita/handlers/todo.rb
@@ -3,17 +3,23 @@ require "lita"
 module Lita
   module Handlers
     class Todo < Handler
-      route(/^todo\wlist$/, :list)
-      route(/^todo\s+add\s+(?<task>)$/, :add)
-      route(/^todo\s+delete\s+(?<task>)$/, :delete)
+      route(/^todo$/, :list, help: {"todo list" => "list my TOOD items"})
+      route(/^todo\s+add\s+(?<task>.*)$/, :add, help: {"todo add task" => "add a todo item"})
+      route(/^todo\s+delete\s+(?<task>\d+)$/, :delete, help: {"todo delete ID" => "delete a todo item"})
 
       def list(response)
         user_id = response.message.source.user.id.to_s
         reply = []
         redis.lrange(user_id, 0, -1).each_with_index do |task, index|
-          reply << "[#{index}] #{task}"
+          unless task.nil? || task == ''
+            reply << "[#{index}] #{task}"
+          end
         end
-        response.reply(reply.join("\n"))
+        if reply.empty?
+          response.reply "todo list is empty"
+        else
+          response.reply(reply.join("\n"))
+        end
       end
 
       def add(response)
@@ -28,7 +34,7 @@ module Lita
         task = response.match_data['task']
         text = redis.lindex user_id, task
         redis.lset(user_id, task, nil)
-        response.reply("TODO #{task} (#{text}) deleted")
+        response.reply("task #{task} (#{text}) deleted")
       end
     end
 
--- spec/lita/handlers/todo_spec.rb
+++ spec/lita/handlers/todo_spec.rb
@@ -1,9 +1,14 @@
 require "spec_helper"
 
 describe Lita::Handlers::Todo, lita_handler: true do
-  it { routes("todo list").to(:list) }
+  it { routes("todo").to(:list) }
   it { routes("todo add test").to(:add) }
   it { routes("todo delete 1").to(:delete) }
+  
+  it 'handles empty todo' do
+    send_message("todo")
+    expect(replies.last).to eq "todo list is empty"
+  end
 
   it "adds tasks" do
     send_message("todo add test")
@@ -12,14 +17,17 @@ describe Lita::Handlers::Todo, lita_handler: true do
 
   it "shows tasks" do
     send_message("todo add test")
+    expect(replies.last).to eq "task 0 added"
     send_message("todo")
     expect(replies.last).to eq "[0] test"
   end
 
   it "deletes tasks" do
     send_message("todo add test")
+    expect(replies.last).to eq "task 0 added"
     send_message("todo delete 0")
+    expect(replies.last).to eq "task 0 (test) deleted"
     send_message("todo")
-    expect(replies.last).to eq "[0] test"
+    expect(replies.last).to eq "todo list is empty"
   end
 end
